72298706b86e0e767356400b2dc2d3ad3c0c8023
Arthur Maziviero <arthur_mfaria@hotmail.com>
2026-04-27 11:29:34 -0300

    criei a funcao de classificacao de aluno

--- src/test.c
+++ src/test.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
 int main (){
-float numero1;
-
-printf("digite a primeira nota: ");
-scanf("%f", &numero1);
-
+int opcao;
+float CalcularMedia(float n1, float n2, float n3){
+    return (n1 + n2 + n3) / 3;
+}
+float ClassificarAluno(float media){
+    if(media >= 7){
+        printf("Aprovado\n")
+    }
+    else if(media <= 7 and >= 5){
+        printf("Recuperacao\n")
+    }
+    else{
+        printf("Recuperacao\n")
+    }
+}
 return 0;
-
 }
